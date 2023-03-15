8ac03bfd5d9990e88495a56ea486d79b2f58bc43
Fer Aguirre <kay_op@protonmail.com>
2023-03-15 08:55:53 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Customized Headlines',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Proof-of-concept to create customized headlines from news content based on demographic data.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
