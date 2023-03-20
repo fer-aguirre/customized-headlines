1690cdfa2ce499161cd5e0058a2114dfa06deefc
Fer Aguirre <kay_op@protonmail.com>
2023-03-19 23:53:28 -0600

    Format functions to load data

--- customized_headlines/data/load.py
+++ customized_headlines/data/load.py
@@ -9,22 +9,26 @@ data_processed = paths.data_processed_dir()
 
 
 # Outputs
-news_sample = paths.data_processed_dir('news_sample.csv')
-news_content = paths.data_processed_dir('news_content.csv')
-
+news = paths.data_processed_dir("news.csv")
+embeddings_t5 = paths.data_processed_dir("embeddings-t5.csv")
+embeddings_ada = paths.data_processed_dir("embeddings-ada.csv")
 
 """
 Function to get csv files
 from the path provided
 """
+
+
 def get_csv_dir(dir: Union[PosixPath, str]):
     # Get the csv files from the path provided
-    return Path(dir).glob('*.csv')
+    return Path(dir).glob("*.csv")
 
 
 """
 Function to read csv 
 file a dataframe
 """
+
+
 def read_csv(file_name: Union[PosixPath, str]):
     return pd.read_csv(file_name)
