2a89ff6eb08a7fc0a0e02b7e206166b51dcc0e12
Fer Aguirre <kay_op@protonmail.com>
2023-03-19 23:54:02 -0600

    Format functions to export data

--- customized_headlines/data/export.py
+++ customized_headlines/data/export.py
@@ -7,5 +7,7 @@ from typing import Union
 Function to save dataframe
 as csv file
 """
+
+
 def df_csv(df: pd.DataFrame, path: Union[pathlib.PosixPath, str]):
     return df.to_csv(path, index=False)
