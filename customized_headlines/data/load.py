5e53257f05c53867142618d2d6f2bd4b6ece26eb
Fer Aguirre <kay_op@protonmail.com>
2023-04-24 23:04:47 -0600

    Add functions to load data

--- customized_headlines/data/load.py
+++ customized_headlines/data/load.py
@@ -13,22 +13,3 @@ news = paths.data_processed_dir("news.csv")
 embeddings_t5 = paths.data_processed_dir("embeddings-t5.csv")
 embeddings_ada = paths.data_processed_dir("embeddings-ada.csv")
 
-"""
-Function to get csv files
-from the path provided
-"""
-
-
-def get_csv_files(dir: Union[PosixPath, str]):
-    # Get the csv files from the path provided
-    return Path(dir).glob("*.csv")
-
-
-"""
-Function to read csv 
-file a dataframe
-"""
-
-
-def read_csv(file_name: Union[PosixPath, str]):
-    return pd.read_csv(file_name)
